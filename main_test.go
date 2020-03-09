4283e7c851121f44514befee5742dafd30defaff
Daniel Martí <mvdan@mvdan.cc>
2020-03-09 19:43:28 +0000

    replace some testscript setup logic with gotooltest
    Thanks to Roger Peppe for the awesome little package.

--- main_test.go
+++ main_test.go
@@ -12,6 +12,7 @@ import (
 	"runtime"
 	"testing"
 
+	"github.com/rogpeppe/go-internal/gotooltest"
 	"github.com/rogpeppe/go-internal/testscript"
 )
 
@@ -30,7 +31,7 @@ func TestScripts(t *testing.T) {
 
 	t.Parallel()
 
-	testscript.Run(t, testscript.Params{
+	p := testscript.Params{
 		Dir: filepath.Join("testdata", "scripts"),
 		Setup: func(env *testscript.Env) error {
 			bindir := filepath.Join(env.WorkDir, ".bin")
@@ -47,27 +48,15 @@ func TestScripts(t *testing.T) {
 			env.Vars = append(env.Vars, fmt.Sprintf("PATH=%s%c%s", bindir, filepath.ListSeparator, os.Getenv("PATH")))
 			env.Vars = append(env.Vars, "TESTSCRIPT_COMMAND=dockexec")
 
-			// GitHub Actions doesn't define %LocalAppData% on
-			// Windows, which breaks $GOCACHE. Set it ourselves.
-			if runtime.GOOS == "windows" {
-				env.Vars = append(env.Vars, fmt.Sprintf(`LOCALAPPDATA=%s\appdata`, env.WorkDir))
-			}
-
 			// In order to make go available inside containers where the guest
 			// and host OS and arch match, we define HOST_GOROOT
 			env.Vars = append(env.Vars, "HOST_GOROOT="+runtime.GOROOT())
-
-			for _, name := range [...]string{
-				"HOME",
-				"USERPROFILE", // $HOME for windows
-				"GOCACHE",
-			} {
-				if value := os.Getenv(name); value != "" {
-					env.Vars = append(env.Vars, name+"="+value)
-				}
-			}
 			return nil
 		},
 		UpdateScripts: *update,
-	})
+	}
+	if err := gotooltest.Setup(&p); err != nil {
+		t.Fatal(err)
+	}
+	testscript.Run(t, p)
 }
